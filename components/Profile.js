ef4e62014b232cc3962e89e16ed8ba21fe972688
Ulferik <alfonsson@yahoo.com>
2023-04-19 10:54:15 +0200

    All

---
+++ components/Profile.js
@@ -0,0 +1,16 @@
+﻿import Image from 'next/image'
+
+export default function Profile(props) {
+    return(
+    <>
+    <Image
+        priority
+        src="/posts/img/profile.png"
+        className='rounded-full pt-3'
+        height={140}
+        width={140}
+        alt="text"
+    />
+    </>
+    )
+}
--- utils/global-data.js
+++ utils/global-data.js
@@ -1,7 +1,7 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Ulferik Home';
+    : "Ulf's Blog";
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
     : "Ulf's blog";
@@ -11,7 +11,7 @@ export const getGlobalData = () => {
 
   return {
     name,
-    blogTitle,
+    // blogTitle,
     footerText,
   };
 };
