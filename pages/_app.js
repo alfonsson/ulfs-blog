4c395e4b333ac315240b9199633d0d02af429fce
Ulferik <alfonsson@yahoo.com>
2023-04-19 10:53:10 +0200

    Script ? _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import '../styles/globals.css';
 import 'prismjs/themes/prism-tomorrow.css';
+import Script from 'next/script';
+
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script src="https://cdn.skypack.dev/remark-gfm@1.0.0" />
       <span className="theme-bejamas" />
       <Component {...pageProps} />
     </>
