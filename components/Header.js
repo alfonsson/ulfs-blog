ab21a29c45b5dc395be50c2447f9ab8d4637dbea
Ulferik <alfonsson@yahoo.com>
2023-06-05 16:52:53 +0200

    Escaped ' &apos;

--- components/Header.js
+++ components/Header.js
@@ -6,11 +6,11 @@ export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
       <div className='pb-5'>
-        <p className='text-left italic text-3xl'>Ulferik's website</p>
+        <p className='text-left italic text-3xl'>Ulferik&apos;s website</p>
         <p className='text-center italic text-2xl p-3'>Gothenburg</p>
         <Profile />
         </div>
-      <div className="w-12 h-8  rounded-full block mx-auto mb-4 bg-gray-400">
+      <div className="w-12 h-8 rounded-full block mx-auto mb-4 bg-gray-400">
         <Link href="/">
           <a className='pl-3'>🏠</a>
         </Link>
