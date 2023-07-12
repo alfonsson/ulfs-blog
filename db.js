09f404381b8df2b3fe901c839b55b6a8b211dc08
Ulferik <alfonsson@yahoo.com>
2023-07-12 14:59:11 +0200

    changed db to mongo+srv

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import { MongoClient } from 'mongodb';
 
 const uri = 'mongodb+srv://Cluster67311:bFduZU5pQWV0@cluster67311.kz0gjtm.mongodb.net/?authMechanism=DEFAULT'
 
-// const uri = 'mongodb://127.0.0.1:27017'; // Replace with your MongoDB connection string
+//const uri = 'mongodb://127.0.0.1:27017'; // Replace with your MongoDB connection string
 const dbName = 'articles'; // Replace with your database name
 
 let cachedClient = null;
